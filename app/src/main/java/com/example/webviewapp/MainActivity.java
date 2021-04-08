999b8291854127f2eabf22fd1b59cc57b7f4f6fe
a17jespe <a17jespe@student.his.se>
2021-04-08 08:57:39 +0200

    Pass to function with variable

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -18,25 +18,22 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetJavaScriptEnabled")
-    public void showExternalWebPage(){
+    public void showWebPage(String where){
 
         WebView my_WebView = findViewById(R.id.my_webview); //Connects my_webview to the content webview ID
         my_WebView.getSettings().setJavaScriptEnabled(true); //Enables javascript in my_WebView
-        my_WebView.loadUrl("https://student.his.se"); // Loads external URL
 
-    }
+        if (where == "external"){
+            my_WebView.loadUrl("https://student.his.se"); // Loads external URL
+        }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    public void showInternalWebPage(){
-
-        WebView my_WebView = findViewById(R.id.my_webview); //Connects my_webview to the content webview ID
-        my_WebView.getSettings().setJavaScriptEnabled(true); //Enables javascript in my_WebView
-        my_WebView.loadUrl("file:///android_asset/about.html"); // Loads internal HTML file
+        else if (where == "internal"){
+            my_WebView.loadUrl("file:///android_asset/about.html"); // Loads internal HTML file
+        }
 
     }
 
     private WebView my_WebView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,18 +63,17 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
-        showExternalWebPage(); // Run function to show external webpage.
+        showWebPage("external"); // Run function to show external webpage.
         }
 
         if (id == R.id.action_internal_web) {
-        showInternalWebPage(); // Run function to show internal webpage.
+        showWebPage("internal"); // Run function to show internal webpage.
         }
 
         return super.onOptionsItemSelected(item);
